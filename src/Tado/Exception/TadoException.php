5898c21fbcc7da046ad59769fce36d777bb780fd
Stephan Eizinga <stephan.eizinga@gmail.com>
2017-12-21 11:18:54 +0100

    no message

---
+++ src/Tado/Exception/TadoException.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tado\Exception;
+
+class TadoException extends Exception
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0)
+    {
+        // make sure everything is assigned properly
+        parent::__construct($message, $code);
+    }
+
+    // custom string representation of object
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
